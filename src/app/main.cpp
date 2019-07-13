de7305fb9cea407e06ca879e3c916a162c02de46
Cyrus Liu <cyrus.xy.l@gmail.com>
2019-07-13 19:26:19 -0400

    Time performance

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,13 +1,15 @@
-#include "router.hpp"
-
+#include <chrono>
 #include <iostream>
+#include "router.hpp"
 
 int main(int /*argc*/, char** /*argv*/)
 {
     router::Router router{10, 10, {0, 0}};
     router::Request request1{0, "cyrus", {0, 1}, {5, 5}};
-    router::Request request2{1, "jess", {5, 6}, {0, 1}};
-
+    router::Request request2{1, "jess", {0, 2}, {5, 5}};
+    auto t1 = std::chrono::system_clock::now();
     router.navigate({request1, request2});
+    auto t2 = std::chrono::system_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count() << '\n';
     return EXIT_SUCCESS;
 }
